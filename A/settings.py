b24d059eaa6052f34eabd7be25e7807a048a5308
Mohammadreza <mohammadrezashokri084@gmail.com>
2026-02-14 22:05:40 +0330

    fixed something security

--- A/settings.py
+++ A/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ru*c6yp*)x^es&tjj0_o#o=9n7o_8lz$9mll50t8-*c^53*79r'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -218,7 +218,7 @@ SIMPLE_JWT = {
     "BLACKLIST_AFTER_ROTATION": False,
     "UPDATE_LAST_LOGIN": False,
 
-    "ALGORITHM": "HS256",
+    "ALGORITHM": config('ALGORITHM'),
     "SIGNING_KEY": SECRET_KEY,
     "VERIFYING_KEY": "",
     "AUDIENCE": None,
